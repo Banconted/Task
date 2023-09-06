1ce06e39d895b63cd84a6737698425bc8579be17
Adel <adel.galeev@bk.ru>
2023-09-06 22:08:01 +0300

    task41

---
+++ task41/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3
+*/
+
+Console.Write("Сколько цифр планируете ввести?: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[m];
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write("Введите число с индексом " + i + ": ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+    if (array[i] > 0) count++;
+}
+Console.Write(String.Join(", ", array) + " -> " + count);
